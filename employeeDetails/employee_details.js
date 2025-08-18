5ba4a4f43c0f04ea8a7bfb9d4200894d25986212
Islam-HH <islamhesham196@outlook.com>
2025-08-18 06:18:26 -0400

    Adding Employees Details feature

---
+++ employeeDetails/employee_details.js
@@ -0,0 +1,63 @@
+const employees = [
+    { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000 },
+    { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000 },
+    { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000 },
+];
+
+function displayEmployees(){
+
+    const totalEmployees = employees.map(totalEmployeesDetails)
+
+    function totalEmployeesDetails(employee){
+        return `<p> ${employee.id}: ${employee.name} - ${employee.age} - ${employee.department} - $${employee.salary}</p>`;
+    }
+
+    document.getElementById('employeesDetails').innerHTML = totalEmployees.join('');
+
+}
+
+function calculateTotalSalaries(){
+
+    const totalSalaries = employees.reduce(totalEmployeesSalaries, 0);
+
+    function totalEmployeesSalaries (total, employee){
+        return total += employee.salary;
+    }
+
+    document.getElementById('employeesDetails').innerHTML = `<p> Total Salaries is $${totalSalaries}</p>`;
+
+}
+
+function displayHREmployees(){
+
+    const HREmployees = employees.filter(findHREmployees);
+
+    function findHREmployees(employee){
+        return employee.department == "HR"; 
+    }
+
+    const HREmployeesDisplay = HREmployees.map(displayHR);
+
+    function displayHR(employee){
+        return `<p> ${employee.id}: ${employee.name} - ${employee.age} - ${employee.department} - $${employee.salary} </p>`;
+    }
+
+    document.getElementById('employeesDetails').innerHTML = HREmployeesDisplay;
+    
+}
+
+function findEmployeeById(employeeId){
+
+    const foundEmployee = employees.find(findEmployee);
+
+    function findEmployee (employee){
+        return employee.id === employeeId;
+    }
+    
+    if (foundEmployee.id === employeeId){
+        document.getElementById('employeesDetails').innerHTML = `<p> ${foundEmployee.id}: ${foundEmployee.name} - ${foundEmployee.age} - ${foundEmployee.department} - $${foundEmployee.salary}</p>`;
+    }
+    else {
+        document.getElementById('employeesDetails').innerHTML = `<p>no employee has been found with this ID`;
+    }
+}
